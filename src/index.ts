6a5949927a1bca9ad22a40dc82fe79b2029098e0
agentg2007 <allen.gepulla@gmail.com>
2022-06-13 00:06:59 +0800

    Beta1.4

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ValidationContextProvider } from "./component";
 
 export { withValidation } from "./hoc";
+export { InputComponentType } from "./models"
 
 export default ValidationContextProvider;
